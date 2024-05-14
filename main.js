b67d743eeb8d0bfa4377aa1bc337febf749d0ab1
KingNoran <kingnoran8@gmail.com>
2024-05-14 21:17:34 +0800

    Polished some stuff. Made a dropDown!

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const imgObserver = new IntersectionObserver((entry)=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('img-slide');
-        } else {
-            entry.target.classList.remove('img-slide');
-        }
-    });
+const burger = document.querySelector('.hamburger');
+const dropDown = document.querySelector('.dropDown');
 
-const mainImg = document.querySelector('.main-img');
-const mainTxt = document.querySelector('.main-text');
-
-mainImg.map((el) => imgObserver.observe(el));
+burger.addEventListener('click', ()=>{
+    dropDown.classList.toggle('show');
+});
