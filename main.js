f4d5d760545fc25f8d027b784b16c53fde2644f8
KingNoran <kingnoran8@gmail.com>
2024-05-15 21:19:06 +0800

    Added the necessary pages and polished things.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const burger = document.querySelector('.hamburger');
+const burgerLogo = document.querySelector('svg.burger');
+const logo = document.querySelector('#logo');
 const dropDown = document.querySelector('.dropDown');
 
 burger.addEventListener('click', ()=>{
     dropDown.classList.toggle('show');
+    if(dropDown.classList.contains('show')){
+        burgerLogo.style["color"] = "#FD9F05"
+    } else {
+        burgerLogo.style["color"] = ""
+    }
+});
+
+logo.addEventListener('click', ()=>{
+    window.location.assign(`${window.location.origin}`);
 });
