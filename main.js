f92765fc314aad93427971c279f60065ca19e3aa
KingNoran <kingnoran8@gmail.com>
2024-05-18 17:51:20 +0800

    Gave it a new color scheme. Made CnP!

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+// Declarations
+
 const burger = document.querySelector('.hamburger');
 const burgerLogo = document.querySelector('svg.burger');
 const logo = document.querySelector('#logo');
 const dropDown = document.querySelector('.dropDown');
+
+//Main Page
 
 burger.addEventListener('click', ()=>{
     dropDown.classList.toggle('show');
@@ -13,5 +17,32 @@ burger.addEventListener('click', ()=>{
 });
 
 logo.addEventListener('click', ()=>{
-    window.location.assign(`${window.location.origin}/Xcite/`);
+    window.location.assign(`${window.location.origin}`);
+});
+
+// Courses and Projects
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) currentIndex = 0;
+    if (index < 0) currentIndex = slides.length - 1;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[currentIndex].style.display = 'flex';
+}
+
+function nextSlide() {
+    showSlide(++currentIndex);
+}
+
+function prevSlide() {
+    showSlide(--currentIndex);
+}
+
+// Initialize slider
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex);
 });
